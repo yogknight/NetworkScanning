8103eb9a61af2bf1f3e1146275460c812c1aa45a
Toykang <qianyunkang@gmail.com>
2018-12-03 15:07:47 +0800

    font

--- UI_Tk.py
+++ UI_Tk.py
@@ -8,7 +8,7 @@ from tkinter import Menu, Tk, messagebox, ttk
 
 from PIL import Image, ImageTk
 
-TITLE_FONT = ("Helvetica", 18, "bold")
+TITLE_FONT = ("Helvetica", 16, "bold")
 FALSE = False
 
 # images
@@ -30,18 +30,18 @@ class Network_Test(Tk):
 
         mainframe = ttk.Frame(self, padding=(3, 3, 12, 12),
                               borderwidth=2, relief='sunken')
-        
+
         mainframe.grid(column=0, row=0, sticky="nwes")
         self.columnconfigure(0, weight=1)
         self.rowconfigure(0, weight=1)
-        self.hig=[]
-        for i in range(0,18):
-                hi=mainframe.rowconfigure(i, weight=1)
-                self.hig.append(hi)
-        self.big=[]
-        for j in range(0,25):
-                tc = mainframe.columnconfigure(j, weight=1)
-                self.big.append(tc)
+        self.hig = []
+        for i in range(0, 18):
+            hi = mainframe.rowconfigure(i, weight=1)
+            self.hig.append(hi)
+        self.big = []
+        for j in range(0, 25):
+            tc = mainframe.columnconfigure(j, weight=1)
+            self.big.append(tc)
         # self.geometry("600x300")
         self.frames = {}
         for F in (StartPage, Network_scan, About_page):
@@ -90,17 +90,18 @@ class Network_scan(ttk.Frame):
         self.mainframe = mainframe
         self.IPtest_img = ImageTk.PhotoImage(IPtest_image)
         self.IPtest = ttk.Label(self, text='局域网地址段扫描',
-                           image=self.IPtest_img, compound='left')
-        
-        self.Ip_start = ttk.Label(self, text='开始地址：',compound='left')
-        self.Ip_end = ttk.Label(self, text='结束地址：',compound='left')
+                                image=self.IPtest_img, compound='left',font=TITLE_FONT,foreground='#1296db')
+
+        self.Ip_start = ttk.Label(self, text='开始地址：', compound='left')
+        self.Ip_end = ttk.Label(self, text='结束地址：', compound='left')
         self.var = StringVar()
         self.Ip_Entry_s = ttk.Entry(self)
-        self.Ip_Entry_e = ttk.Entry(self,textvariable=self.var)
-        
+        self.Ip_Entry_e = ttk.Entry(self, textvariable=self.var)
 
-        self.get_end_IP=ttk.Button(self,text="自动",command=lambda: self.set_end_ip())
-        self.Do_scanning=ttk.Button(self, text="开始扫描",command=lambda: self.start_ping())
+        self.get_end_IP = ttk.Button(
+            self, text="自动", command=lambda: self.set_end_ip())
+        self.Do_scanning = ttk.Button(
+            self, text="开始扫描", command=lambda: self.start_ping())
         # 网段地址图标
         self.list_index = 0
         self.label_list = []
@@ -109,29 +110,30 @@ class Network_scan(ttk.Frame):
                 self.label = ttk.Label(
                     self, text=self.list_index, background="#CBCBCB")
                 self.list_index += 1
-                self.label.grid(column=j, row=i, sticky="nwes",padx=5, pady=5)
+                self.label.grid(column=j, row=i, sticky="nwes", padx=5, pady=5)
                 self.label_list.append(self.label)
 
         # 界面布局
-        self.IPtest.grid(column=0, row=0, sticky="nwes",padx=5, pady=5)
-        self.Ip_Entry_s.grid(column=0, row=2, sticky="nwes",padx=5, pady=5)
-        self.Ip_start.grid(column=0, row=1, sticky="nwes",padx=5, pady=5)
-        self.Ip_end.grid(column=0, row=3, sticky="nwes",padx=5, pady=5)
-        self.get_end_IP.grid(column=1, row=4, sticky="nwes",padx=5, pady=5)
-        self.Ip_Entry_e.grid(column=0, row=4, sticky="nwes",padx=5, pady=5)
-        self.Do_scanning.grid(column=0, row=5, sticky="nwes",padx=5, pady=5)
+        self.IPtest.grid(column=0, row=0, sticky="nwes", padx=5, pady=5)
+        self.Ip_Entry_s.grid(column=0, row=2, sticky="nwes", padx=5, pady=5)
+        self.Ip_start.grid(column=0, row=1, sticky="nwes", padx=5, pady=5)
+        self.Ip_end.grid(column=0, row=3, sticky="nwes", padx=5, pady=5)
+        self.get_end_IP.grid(column=1, row=4, sticky="nwes", padx=5, pady=5)
+        self.Ip_Entry_e.grid(column=0, row=4, sticky="nwes", padx=5, pady=5)
+        self.Do_scanning.grid(column=0, row=5, sticky="nwes", padx=5, pady=5)
+
     def set_end_ip(self):
         """
         填写起始地址后，默认填写结束地址为xxx.xxx.xxx.255
         """
-        startip =self.Ip_Entry_s.get()
+        startip = self.Ip_Entry_s.get()
         pattern = r"((?:(?:25[0-5]|2[0-4]\d|((1\d{2})|([1-9]?\d)))\.){3}(?:25[0-5]|2[0-4]\d|((1\d{2})|([1-9]?\d)))$)"
         m = re.match(pattern, startip)      # 检查IP地址是否合法
         if m:
             startip = startip.split('.')
             startip[3] = '255'
             endip = '.'.join(startip)
-            endip =self.var.set(endip)
+            endip = self.var.set(endip)
         else:
             messagebox.showinfo(message='IP地址错误！\n地址只能为一个网段的IP，请检查你的输入！')
 
@@ -139,32 +141,34 @@ class Network_scan(ttk.Frame):
         '''
         启动多线程
         '''
-        #检测截至IP
-        endip =self.var.get()
+        # 检测截至IP
+        endip = self.var.get()
         pattern = r"((?:(?:25[0-5]|2[0-4]\d|((1\d{2})|([1-9]?\d)))\.){3}(?:25[0-5]|2[0-4]\d|((1\d{2})|([1-9]?\d)))$)"
         m = re.match(pattern, endip)      # 检查IP地址是否合法
         if m:
-            end_ip_test=True
+            end_ip_test = True
         else:
-            end_ip_test=False
+            end_ip_test = False
             messagebox.showinfo(message='IP地址错误！\n 详细信息：\n结束地址错误，请检查你的输入！')
-        
-        #开始测试
+
+        # 开始测试
         self.reset_ui()
-        startip =self.Ip_Entry_s.get().split('.')
-        endip =self.var.get().split('.')
+        startip = self.Ip_Entry_s.get().split('.')
+        endip = self.var.get().split('.')
         tmp_ip = startip
-        if int(startip[3])<=int(endip[3]) and end_ip_test:
+        if int(startip[3]) <= int(endip[3]) and end_ip_test:
             pthread_list = []
             for i in range(int(startip[3]), int(endip[3]) + 1):
                 tmp_ip[3] = str(i)
                 ip = '.'.join(tmp_ip)
-                pthread_list.append(threading.Thread(target=self.get_ping_result, args=(ip,)))
+                pthread_list.append(threading.Thread(
+                    target=self.get_ping_result, args=(ip,)))
             for item in pthread_list:
                 item.setDaemon(True)
                 item.start()
-        elif end_ip_test and int(startip[3])>int(endip[3]):
-            messagebox.showinfo(message='IP地址错误！\n详细信息：\n结束地址需要大于开始地址，请检查你的输入！')
+        elif end_ip_test and int(startip[3]) > int(endip[3]):
+            messagebox.showinfo(
+                message='IP地址错误！\n详细信息：\n结束地址需要大于开始地址，请检查你的输入！')
 
     def get_ping_result(self, ip):
         """
@@ -173,7 +177,8 @@ class Network_scan(ttk.Frame):
         cmd_str = "ping {0} -n 3 -w 600".format(ip)
         DETACHED_PROCESS = 0x00000008   # 不创建cmd窗口
         try:
-            subprocess.run(cmd_str, creationflags=DETACHED_PROCESS, check=True)  # 仅用于windows系统
+            subprocess.run(cmd_str, creationflags=DETACHED_PROCESS,
+                           check=True)  # 仅用于windows系统
         except subprocess.CalledProcessError as err:
             self.set_ui(False, ip)
         else:
@@ -184,7 +189,7 @@ class Network_scan(ttk.Frame):
         初始化窗口IP窗格为灰色背景
         """
         for item in self.label_list:
-            item['background']="#CBCBCB"
+            item['background'] = "#CBCBCB"
 
     def set_ui(self, result, ip):
         """
@@ -194,11 +199,11 @@ class Network_scan(ttk.Frame):
         """
         index = int(ip.split('.')[3])
         if result:
-            self.label_list[index]['background']="#55AA7F" # 设置背景为绿色
+            self.label_list[index]['background'] = "#55AA7F"  # 设置背景为绿色
         else:
-            self.label_list[index]['background']="#FF8E77"   # 设置背景为红色
+            self.label_list[index]['background'] = "#FF8E77"   # 设置背景为红色
 
-        
+
 class About_page(ttk.Frame):
 
     def __init__(self, parent, mainframe):
@@ -206,24 +211,27 @@ class About_page(ttk.Frame):
         self.mainframe = mainframe
         self.about_img = ImageTk.PhotoImage(about_image)
         self.Github_img = ImageTk.PhotoImage(Github_image)
-        self.About = ttk.Label(self, text='关于', image=self.about_img, compound='left')
-        self.About_info1=ttk.Label(self, text='版本：0.1')
-        self.About_info2=ttk.Label(self, text='提交：')
-        self.About_info3=ttk.Label(self, text='日期：')
-        self.About_info4=ttk.Label(self, text='源码：源码发布于GiHub')
-        
-        self.About_source=ttk.Button(self,image=self.Github_img,command=lambda: self.web_view())
-        
-        self.About.grid(column=0, row=0, sticky="nwes",padx=5, pady=5)
-        self.About_info1.grid(column=1, row=1, sticky="nwes",padx=5, pady=5)
-        self.About_info2.grid(column=1, row=2, sticky="nwes",padx=5, pady=5)
-        self.About_info3.grid(column=1, row=3, sticky="nwes",padx=5, pady=5)
-        self.About_info4.grid(column=1, row=4, sticky="nwes",padx=5, pady=5)
-        self.About_source.grid(column=2, row=4, sticky="nwes",padx=5, pady=5)
+        self.About = ttk.Label(
+            self, text='关于', image=self.about_img, compound='left',font=TITLE_FONT,foreground='#1296db')
+        self.About_info1 = ttk.Label(self, text='版本：0.1(Toykang)')
+        self.About_info2 = ttk.Label(self, text='提交：1')
+        self.About_info3 = ttk.Label(
+            self, text='日期：2018, 12, 3, 14, 27, 9, 402166 ')
+        self.About_info4 = ttk.Label(self, text='源码：源码发布于GiHub，更多详细信息请点击')
+
+        self.About_source = ttk.Button(
+            self, image=self.Github_img, command=lambda: self.web_view())
+
+        self.About.grid(column=0, row=0, sticky="nwes", padx=5, pady=5)
+        self.About_info1.grid(column=1, row=1, sticky="nwes", padx=5, pady=5)
+        self.About_info2.grid(column=1, row=2, sticky="nwes", padx=5, pady=5)
+        self.About_info3.grid(column=1, row=3, sticky="nwes", padx=5, pady=5)
+        self.About_info4.grid(column=1, row=4, sticky="nwes", padx=5, pady=5)
+        self.About_source.grid(column=2, row=4, sticky="nwes", padx=5, pady=5)
+
     def web_view(self):
         webbrowser.open("https://github.com/Toykang/NetworkScanning")
 
-    
 
 if __name__ == "__main__":
     app = Network_Test()
