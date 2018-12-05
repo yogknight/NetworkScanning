4063fc7548576d20ed85129e34ac16f8723e9c27
Toykang <qianyunkang@gmail.com>
2018-12-05 15:30:25 +0800

    ADD AutoScanning tool

--- TK_NetworkScanning.py
+++ TK_NetworkScanning.py
@@ -3,21 +3,35 @@ import subprocess
 import sys
 import threading
 import webbrowser
-from tkinter import *
-from tkinter import Menu, Tk, messagebox, ttk
+from tkinter import Menu, Tk, messagebox, ttk, StringVar, Listbox
+from tkinter.filedialog import askopenfilename, asksaveasfilename
 
 from PIL import Image, ImageTk
 
 TITLE_FONT = ("Helvetica", 16, "bold")
 FALSE = False
+function = 'function'
 
 # images
 img_about = Image.open('about_img.png')
 img_IPtest = Image.open('IPtest_img.png')
 img_Github = Image.open('GitHub_img.png')
+img_ALL_IPimg = Image.open('ALL_IP_img.png')
+img_infile = Image.open('infile_img.png')
+img_outFile = Image.open('outFile_img.png')
+img_go = Image.open('go_img.png')
+
+# 定义图片尺寸
 about_image = img_about.resize((60, 60), Image.ANTIALIAS)
 IPtest_image = img_IPtest.resize((60, 60), Image.ANTIALIAS)
 Github_image = img_Github.resize((10, 10), Image.ANTIALIAS)
+ALL_IPimg_image = img_ALL_IPimg.resize((60, 60), Image.ANTIALIAS)
+
+# 导入导出
+infile_image = img_infile.resize((25, 25), Image.ANTIALIAS)
+outFile_image = img_outFile.resize((25, 25), Image.ANTIALIAS)
+
+go_image = img_go.resize((25, 25), Image.ANTIALIAS)
 
 
 class Network_Test(Tk):
@@ -44,7 +58,7 @@ class Network_Test(Tk):
             self.big.append(tc)
         # self.geometry("600x300")
         self.frames = {}
-        for F in (StartPage, Network_scan, About_page):
+        for F in (StartPage, Network_scan, About_page, ALL_IPtest):
             page_name = F.__name__
             frame = F(parent=mainframe, mainframe=self)
             self.frames[page_name] = frame
@@ -81,16 +95,21 @@ class StartPage(ttk.Frame):
             label='关于(About)', command=lambda: mainframe.show_frame("About_page"))
         menu_tools.add_command(label='网段扫描(Network scanning)',
                                command=lambda: mainframe.show_frame("Network_scan"))
+        menu_tools.add_command(label='自定义扫描(Auto Test)',
+                               command=lambda: mainframe.show_frame("ALL_IPtest"))
 
 
 class Network_scan(ttk.Frame):
+    """
+    网段扫描工具
+    """
 
     def __init__(self, parent, mainframe):
         ttk.Frame.__init__(self, parent)
         self.mainframe = mainframe
         self.IPtest_img = ImageTk.PhotoImage(IPtest_image)
-        self.IPtest = ttk.Label(self, text='局域网地址段扫描',
-                                image=self.IPtest_img, compound='left',font=TITLE_FONT,foreground='#1296db')
+        self.IPtest = ttk.Label(self, text='地址段扫描',
+                                image=self.IPtest_img, compound='left', font=TITLE_FONT, foreground='#1296db')
 
         self.Ip_start = ttk.Label(self, text='开始地址：', compound='left')
         self.Ip_end = ttk.Label(self, text='结束地址：', compound='left')
@@ -102,6 +121,14 @@ class Network_scan(ttk.Frame):
             self, text="自动", command=lambda: self.set_end_ip())
         self.Do_scanning = ttk.Button(
             self, text="开始扫描", command=lambda: self.start_ping())
+        self.choie_num = ttk.Label(self, text="选择测试次数：", compound='left')
+
+        # 选择ping次数
+        self.countryvar = StringVar()
+        self.country = ttk.Combobox(self, textvariable=self.countryvar)
+        self.country.bind('<< ComboboxSelected >>', function)
+        self.country['values'] = ('1', '2', '3', '4', '5')
+        self.countryvar.set('3')
         # 网段地址图标
         self.list_index = 0
         self.label_list = []
@@ -120,11 +147,13 @@ class Network_scan(ttk.Frame):
         self.Ip_end.grid(column=0, row=3, sticky="nwes", padx=5, pady=5)
         self.get_end_IP.grid(column=1, row=4, sticky="nwes", padx=5, pady=5)
         self.Ip_Entry_e.grid(column=0, row=4, sticky="nwes", padx=5, pady=5)
-        self.Do_scanning.grid(column=0, row=5, sticky="nwes", padx=5, pady=5)
+        self.choie_num.grid(column=0, row=5, sticky="nwes", padx=5, pady=5)
+        self.country.grid(column=0, row=6, sticky="nwes", padx=5, pady=5)
+        self.Do_scanning.grid(column=0, row=7, sticky="nwes", padx=5, pady=5)
 
     def set_end_ip(self):
         """
-        填写起始地址后，默认填写结束地址为xxx.xxx.xxx.255
+        填写起始地址后，默认填写结束地址为同网段最后一个地址
         """
         startip = self.Ip_Entry_s.get()
         pattern = r"((?:(?:25[0-5]|2[0-4]\d|((1\d{2})|([1-9]?\d)))\.){3}(?:25[0-5]|2[0-4]\d|((1\d{2})|([1-9]?\d)))$)"
@@ -138,9 +167,9 @@ class Network_scan(ttk.Frame):
             messagebox.showinfo(message='IP地址错误！\n地址只能为一个网段的IP，请检查你的输入！')
 
     def start_ping(self):
-        '''
+        """
         启动多线程
-        '''
+        """
         # 检测截至IP
         endip = self.var.get()
         pattern = r"((?:(?:25[0-5]|2[0-4]\d|((1\d{2})|([1-9]?\d)))\.){3}(?:25[0-5]|2[0-4]\d|((1\d{2})|([1-9]?\d)))$)"
@@ -174,7 +203,9 @@ class Network_scan(ttk.Frame):
         """
         检查对应的IP是否被占用
         """
-        cmd_str = "ping {0} -n 3 -w 600".format(ip)
+        num = self.countryvar.get()
+        commands = "ping -n {0} -w 600".format(num)
+        cmd_str = commands+" {0}".format(ip)
         DETACHED_PROCESS = 0x00000008   # 不创建cmd窗口
         try:
             subprocess.run(cmd_str, creationflags=DETACHED_PROCESS,
@@ -204,7 +235,134 @@ class Network_scan(ttk.Frame):
             self.label_list[index]['background'] = "#FF8E77"   # 设置背景为红色
 
 
+class ALL_IPtest(ttk.Frame):
+    """
+    任意IP地址扫描
+    扫描结显示到窗口
+    也可以选择导出到文本文件
+    """
+
+    def __init__(self, parent, mainframe):
+        ttk.Frame.__init__(self, parent)
+        self.mainframe = mainframe
+        # 获取图片
+        self.ALLIP_img = ImageTk.PhotoImage(ALL_IPimg_image)
+        self.infile_img = ImageTk.PhotoImage(infile_image)
+        self.outFile_img = ImageTk.PhotoImage(outFile_image)
+        self.go_img = ImageTk.PhotoImage(go_image)
+        self.IPtest = ttk.Label(self, text='自定义扫描',
+                                image=self.ALLIP_img, compound='left', font=TITLE_FONT, foreground='#1296db')
+        self.Get_IPtxt = ttk.Button(
+            self, text="导入IP文件", image=self.infile_img, compound='left',command=lambda: self.start_ping()())
+        self.Go_Scanning = ttk.Button(
+            self, text="开始扫描", image=self.go_img, compound='left',command=lambda: self.Scanning_show())
+        self.Out_ScanningTxt = ttk.Button(
+            self, text="导出结果", image=self.outFile_img, compound='left',command=lambda: self.save_view())
+        self.TestView = ttk.Label(
+            self, text='扫描结果：', font=TITLE_FONT, foreground='#1296db')
+
+        # 结果显示
+        VERTICAL = "vertical"
+        self.Scanning_L = Listbox(self, height=20, width=100)
+        self.ScanViews = ttk.Scrollbar(
+            self, orient=VERTICAL, command=self.Scanning_L.yview)
+        self.Scanning_L['yscrollcommand'] = self.ScanViews.set
+        ttk.Sizegrip().grid(column=2, row=4, sticky="se")
+        self.Scanning_L.insert('end', 'IP地址               测试次数                通信状态')
+
+
+
+        self.ScanViews.grid(column=30, row=4, sticky="ns")
+        self.Scanning_L.grid(column=2, row=4, sticky="nwes", padx=5, pady=5)
+        self.IPtest.grid(column=0, row=0, sticky="nwes", padx=5, pady=5)
+        self.Get_IPtxt.grid(column=0, row=1, sticky="nwes", padx=5, pady=5)
+        self.Go_Scanning.grid(column=0, row=2, sticky="nwes", padx=5, pady=5)
+        self.Out_ScanningTxt.grid(
+            column=0, row=3, sticky="nwes", padx=5, pady=5)
+        self.TestView.grid(column=2, row=3, sticky="nwes", padx=5, pady=5)
+
+    # 获取IP
+    def check_file(self):
+        """
+         askopenfilename获取IP地址文件
+        """
+        self.open_filename = askopenfilename(
+            title='打开文件', filetypes=[('All Files', '*')])
+        with open(self.open_filename, 'r') as f:
+            self.startip = f.readlines()
+        return(self.startip)
+    # 处理IP
+    def start_ping(self):
+        """
+        启动多线程
+        检查IP地址合法性
+        """
+        get_ALLip=self.check_file()
+        pthread_list = []
+        for line in get_ALLip:
+            if len(line.strip()):
+                ip=line.strip('\n')
+                pattern = r"((?:(?:25[0-5]|2[0-4]\d|((1\d{2})|([1-9]?\d)))\.){3}(?:25[0-5]|2[0-4]\d|((1\d{2})|([1-9]?\d)))$)"
+                m = re.match(pattern, line)      # 检查IP地址是否合法
+                if m:
+                    ip_check = True
+                else:
+                    ip_check = False
+                # 开始测试
+                pthread_list.append(threading.Thread(target=self.get_ping_result, args=(ip,)))
+        for item in pthread_list:
+            item.setDaemon(True)
+            item.start()
+        with open('./IPtest.txt','w+') as f:
+            f.write("+---------------+----------+----------+\n")
+            f.write("|    IP地址     |  扫描次数 |  通信情况 |\n")
+            f.write("+---------------+----------+----------+\n")
+        f.close()
+
+    def get_ping_result(self, ip):
+        """
+        检查对应的IP是否被占用
+        """
+        cmd_str = "ping {0} -n 4 -w 600".format(ip)
+        DETACHED_PROCESS = 0x00000008   # 不创建cmd窗口
+        try:
+            subprocess.run(cmd_str, creationflags=DETACHED_PROCESS,
+                           check=True)  # 仅用于windows系统
+        except subprocess.CalledProcessError as err:
+            self.write_file(False, ip)
+        else:
+            self.write_file(True, ip)
+
+    def write_file(self, result, ip):
+        """
+        将结果写入文件
+        result：线程ping的结果
+        ip：为对于的IP地址
+        """
+        ip=ip.strip('\n')
+        with open('./IPtest.txt','a+') as f:
+            if result:
+                self.Scanning_L.insert('end', '%s               4               通信正常' % ip)
+                f.write("|  {0}  |   4    |   通信正常  |\n".format(ip))
+                f.write("+---------------+----------+----------+\n")
+                self.Scanning_L.update()
+            else:
+                self.Scanning_L.insert('end', '%s               4               通信失败' % ip)
+                f.write("|  {0}  |   4    |   通信失败  |\n".format(ip))
+                f.write("+---------------+----------+----------+\n")
+                self.Scanning_L.update()
+    def save_view(self):
+        r =asksaveasfilename(title='保存文件', initialdir='./', initialfile='table.txt')
+        with open('./IPtest.txt','r+') as f:
+            lines=f.read()
+        with open(r,'a+') as S:
+            S.write(lines)
+        
+
 class About_page(ttk.Frame):
+    """
+    关于APP的详细信息
+    """
 
     def __init__(self, parent, mainframe):
         ttk.Frame.__init__(self, parent)
@@ -212,7 +370,7 @@ class About_page(ttk.Frame):
         self.about_img = ImageTk.PhotoImage(about_image)
         self.Github_img = ImageTk.PhotoImage(Github_image)
         self.About = ttk.Label(
-            self, text='关于', image=self.about_img, compound='left',font=TITLE_FONT,foreground='#1296db')
+            self, text='关于', image=self.about_img, compound='left', font=TITLE_FONT, foreground='#1296db')
         self.About_info1 = ttk.Label(self, text='版本：0.1(Toykang)')
         self.About_info2 = ttk.Label(self, text='提交：1')
         self.About_info3 = ttk.Label(
