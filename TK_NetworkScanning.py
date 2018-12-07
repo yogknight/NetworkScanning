e928d0b9010177b4ac44710a046617b65dff2bbe
Toykang <qianyunkang@gmail.com>
2018-12-07 13:20:02 +0800

    add startpage

--- TK_NetworkScanning.py
+++ TK_NetworkScanning.py
@@ -20,12 +20,14 @@ img_ALL_IPimg = Image.open('ALL_IP_img.png')
 img_infile = Image.open('infile_img.png')
 img_outFile = Image.open('outFile_img.png')
 img_go = Image.open('go_img.png')
+img_one_IPtes=Image.open('one_IPtest_img.png')
 
 # 定义图片尺寸
 about_image = img_about.resize((60, 60), Image.ANTIALIAS)
 IPtest_image = img_IPtest.resize((60, 60), Image.ANTIALIAS)
 Github_image = img_Github.resize((10, 10), Image.ANTIALIAS)
 ALL_IPimg_image = img_ALL_IPimg.resize((60, 60), Image.ANTIALIAS)
+one_IPtest_image = img_one_IPtes.resize((60, 60), Image.ANTIALIAS)
 
 # 导入导出
 infile_image = img_infile.resize((25, 25), Image.ANTIALIAS)
@@ -44,7 +46,7 @@ class Network_Test(Tk):
 
         mainframe = ttk.Frame(self, padding=(3, 3, 12, 12),
                               borderwidth=2, relief='sunken')
-        
+        self.resizable(width=False, height=False)#禁止拉升窗口
         self.iconbitmap(".\\app_ico.ico")
         mainframe.grid(column=0, row=0, sticky="nwes")
         self.columnconfigure(0, weight=1)
@@ -81,7 +83,6 @@ class StartPage(ttk.Frame):
     def __init__(self, parent, mainframe):
         ttk.Frame.__init__(self, parent)
         self.mainframe = mainframe
-
         self.mainframe.title("网络测试(NetworkTest)")
 
         # 菜单栏
@@ -92,12 +93,106 @@ class StartPage(ttk.Frame):
         menu_help = Menu(menubar)
         menubar.add_cascade(menu=menu_tools, label='工具库(Tools)')
         menubar.add_cascade(menu=menu_help, label='帮助(H)')
+        menu_tools.add_command(label='IP地址测试(IP Test)',
+                               command=lambda: mainframe.show_frame("StartPage"))
         menu_help.add_command(
             label='关于(About)', command=lambda: mainframe.show_frame("About_page"))
         menu_tools.add_command(label='网段扫描(Network scanning)',
                                command=lambda: mainframe.show_frame("Network_scan"))
         menu_tools.add_command(label='自定义扫描(Auto Test)',
                                command=lambda: mainframe.show_frame("ALL_IPtest"))
+        
+
+        #单个地址测试
+        self.one_IPtest_img = ImageTk.PhotoImage(one_IPtest_image)
+        self.IPtest = ttk.Label(self, text='IP地址测试',
+                                image=self.one_IPtest_img, compound='left', font=TITLE_FONT, foreground='#1296db')
+
+        self.Ip_start = ttk.Label(self, text='输入地址：', compound='left')
+        self.one_iptest = StringVar()
+        self.one_Ip_Entry = ttk.Entry(self, textvariable=self.one_iptest)
+        self.one_scanning = ttk.Button(
+            self, text="测试",command=lambda: self.One_IPtest())
+
+        self.clear_views = ttk.Button(
+            self, text="清空",command=lambda: self.cleane_view())
+
+        self.Stop_test = ttk.Button(
+            self, text="停止",command=lambda: self.Stop_Popen())
+
+
+        self.choie_N = ttk.Label(self, text="选择测试次数：", compound='left')
+        self.view_title = ttk.Label(self, text="测试结果", compound='left')
+
+        #stop_popen
+        self.stop_IPtest = StringVar()
+        self.stop_IPtest.set('1')
+
+        # 选择ping次数
+        self.count_IPtest = StringVar()
+        self.country_one = ttk.Combobox(self, textvariable=self.count_IPtest)
+        self.country_one.bind('<< ComboboxSelected >>', function)
+        self.country_one['values'] = ('2', '3', '4', '5','∞')
+        self.count_IPtest.set('4')
+
+
+        # 结果显示
+        VERTICAL = "vertical"
+        self.Scanning_one = Listbox(self, height=20, width=100)
+        self.ScanViews_one = ttk.Scrollbar(
+            self, orient=VERTICAL, command=self.Scanning_one.yview)
+        self.Scanning_one['yscrollcommand'] = self.ScanViews_one.set
+        ttk.Sizegrip().grid(column=2, row=4, sticky="se")
+
+
+
+        #布局
+        self.IPtest.grid(column=0, row=0, sticky="nwes", padx=5, pady=5)
+        self.Ip_start.grid(column=1, row=1, sticky="nwes", padx=5, pady=5)
+        self.one_Ip_Entry.grid(column=2, row=1, sticky="nwes", padx=5, pady=5)
+        self.choie_N.grid(column=3, row=1, sticky="nwes", padx=5, pady=5)
+        self.country_one.grid(column=4, row=1, sticky="nwes", padx=5, pady=5)
+        self.one_scanning.grid(column=5, row=1, sticky="nwes", padx=5, pady=5)
+        self.view_title.grid(column=1, row=2, sticky="nwes",padx=5, pady=5)
+        self.ScanViews_one.grid(column=21, row=3, sticky="ns")
+        self.Scanning_one.grid(column=1, row=3, sticky="nwes",columnspan=10,padx=5, pady=5)
+        self.Stop_test.grid(column=1, row=11, sticky="nwes",columnspan=1,rowspan=1,padx=5, pady=5)
+        self.clear_views.grid(column=10, row=11, sticky="nwes",columnspan=1,rowspan=1,padx=5, pady=5)
+    
+
+    #开始ping测试
+    def One_IPtest(self):
+        """
+        获取IP，开始Ping测试，结果实时输出到窗口
+        """
+        one_ip = self.one_iptest.get()#获取IP
+        count_testnum=self.count_IPtest.get()#获取测试次数
+        control=self.stop_IPtest.get()
+        self.stop_IPtest.set('1')
+        if count_testnum=='∞':
+            add_num="ping -t "
+        else:
+            add_num="ping -n {0} ".format(count_testnum)
+        cmd=add_num+"{0}".format(one_ip)
+        p = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, shell = False)
+        while p.poll() is None:
+                line = p.stdout.readline().decode('gbk')
+                line = line.strip()
+                control=self.stop_IPtest.get()
+                if line:
+                    test_out='Subprogram output: [{}]'.format(line)
+                    self.Scanning_one.insert('end',test_out)
+                    self.Scanning_one.update()
+                if control=='0':
+                        p.terminate()
+                
+    
+    def cleane_view(self):
+        self.Scanning_one.delete('0','end')
+    
+    def Stop_Popen(self):
+        self.stop_IPtest.set('0')
+        
 
 
 class Network_scan(ttk.Frame):
@@ -256,7 +351,7 @@ class ALL_IPtest(ttk.Frame):
         self.Get_IPtxt = ttk.Button(
             self, text="导入IP文件", image=self.infile_img, compound='left',command=lambda: self.start_ping()())
         self.Go_Scanning = ttk.Button(
-            self, text="开始扫描", image=self.go_img, compound='left',command=lambda: self.Scanning_show())
+            self, text="开始扫描", image=self.go_img, compound='left')
         self.Out_ScanningTxt = ttk.Button(
             self, text="导出结果", image=self.outFile_img, compound='left',command=lambda: self.save_view())
         self.TestView = ttk.Label(
@@ -273,14 +368,14 @@ class ALL_IPtest(ttk.Frame):
 
 
 
-        self.ScanViews.grid(column=30, row=4, sticky="ns")
-        self.Scanning_L.grid(column=2, row=4, sticky="nwes", padx=5, pady=5)
+        self.ScanViews.grid(column=21, row=3, sticky="ns")
+        self.Scanning_L.grid(column=1, row=3, sticky="nwes",columnspan=20,padx=5, pady=5)
         self.IPtest.grid(column=0, row=0, sticky="nwes", padx=5, pady=5)
-        self.Get_IPtxt.grid(column=0, row=1, sticky="nwes", padx=5, pady=5)
-        self.Go_Scanning.grid(column=0, row=2, sticky="nwes", padx=5, pady=5)
+        self.Get_IPtxt.grid(column=1, row=1, sticky="nwes",columnspan=1,rowspan=1,padx=5, pady=5)
+        self.Go_Scanning.grid(column=2, row=1, sticky="nwes",columnspan=1,rowspan=1, padx=5, pady=5)
         self.Out_ScanningTxt.grid(
-            column=0, row=3, sticky="nwes", padx=5, pady=5)
-        self.TestView.grid(column=2, row=3, sticky="nwes", padx=5, pady=5)
+            column=20, row=20, sticky="nwes",columnspan=1,rowspan=1,padx=5, pady=5)
+        self.TestView.grid(column=1, row=2, sticky="nwes", padx=5, pady=5)
 
     # 获取IP
     def check_file(self):
